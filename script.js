0147994da1a2658c07ab935c379e7b7409791a36
Omar Martin <omarmc89@gmail.com>
2024-01-23 18:58:10 +0100

    sass included

--- script.js
+++
@@ -1,7 +0,0 @@
-// let text = document.getElementById('banner-text')
-// const maxMovement = window.innerWidth - text.offsetWidth
-// window.addEventListener('scroll', () => {
-//   let value = window.scrollY
-//   text.style.opacity = 1 - value / 500
-//   text.style.left = value / 3 + 'px'
-// })
